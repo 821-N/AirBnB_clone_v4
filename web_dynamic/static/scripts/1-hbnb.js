f701fdcb02aec1c0c631be026f15e45633ecac50
821-N <nathaniel.marofsky@gmail.com>
2020-02-17 23:46:39 -0500

    add 1

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+
+function update_amenities() {
+	var checkboxes = document.querySelectorAll(".amenities li input[type=\"checkbox\"]");
+	var names = [], ids = [];
+	if (checkboxes)
+		checkboxes.forEach(x => {
+			if (x.checked) {
+				ids.push(x.id);
+				names.push(x.name);
+			}
+		});
+	$(".amenities > h4").text(names.join(", "));
+}
+
+$(document).ready(function() {
+	update_amenities();
+});
